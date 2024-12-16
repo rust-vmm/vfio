bdbb1cd486484faa23db48c82d51484e2ee43692
Jinank Jain <jinankjain@microsoft.com>
2024-12-16 04:37:00 +0000

    vfio-ioctls: Fix clippy warning
    error: manually reimplementing `div_ceil`
      --> crates/vfio-ioctls/src/fam.rs:12:24
       |
    12 |     let rounded_size = (size_in_bytes + size_of::<T>() - 1) / size_of::<T>();
       |                        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
       help: consider using `.div_ceil()`: `size_in_bytes.div_ceil(size_of::<T>())`
       |
       = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#manual_div_ceil
       = note: `-D clippy::manual-div-ceil` implied by `-D warnings`
       = help: to override `-D warnings` add `#[allow(clippy::manual_div_ceil)]`
    
    Signed-off-by: Jinank Jain <jinankjain@microsoft.com>

--- crates/vfio-ioctls/src/fam.rs
+++ crates/vfio-ioctls/src/fam.rs
@@ -9,7 +9,7 @@ use std::mem::size_of;
 
 /// Returns a `Vec<T>` with a size in bytes at least as large as `size_in_bytes`.
 fn vec_with_size_in_bytes<T: Default>(size_in_bytes: usize) -> Vec<T> {
-    let rounded_size = (size_in_bytes + size_of::<T>() - 1) / size_of::<T>();
+    let rounded_size = size_in_bytes.div_ceil(size_of::<T>());
     let mut v = Vec::with_capacity(rounded_size);
     for _ in 0..rounded_size {
         v.push(T::default())

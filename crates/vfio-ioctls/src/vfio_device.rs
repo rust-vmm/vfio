f3a1ae0f601c13417c8a5164e688438e92181825
Muminul Islam <muislam@microsoft.com>
2025-04-03 13:52:39 -0700

    vfio-ioctls: fix build error for MSHV
    Signed-off-by: Muminul Islam <muislam@microsoft.com>

--- crates/vfio-ioctls/src/vfio_device.rs
+++ crates/vfio-ioctls/src/vfio_device.rs
@@ -37,6 +37,8 @@ use mshv_bindings::{
 use mshv_ioctls::DeviceFd as MshvDeviceFd;
 #[cfg(all(any(feature = "kvm", feature = "mshv"), not(test)))]
 use std::os::unix::io::FromRawFd;
+#[cfg(all(feature = "mshv", not(test)))]
+use vmm_sys_util::errno::Error;
 
 #[derive(Debug)]
 enum DeviceFdInner {
@@ -379,7 +381,7 @@ impl VfioContainer {
                         addr: group_fd_ptr as u64,
                     };
                     fd.set_device_attr(&dev_attr)
-                        .map_err(|e| VfioError::SetDeviceAttr(e.into()))
+                        .map_err(|e| VfioError::SetDeviceAttr(Error::new(e.errno())))
                 }
             }
         } else {

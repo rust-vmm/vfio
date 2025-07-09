16db42386def38d57d262a328d0f8ce62340b2d4
Bo Chen <bchen@crusoe.ai>
2025-07-09 18:06:13 +0000

    vfio-bindings: Remove unnecessary linting attribute
    Signed-off-by: Bo Chen <bchen@crusoe.ai>

--- vfio-bindings/src/vfio_bindings/mod.rs
+++ vfio-bindings/src/vfio_bindings/mod.rs
@@ -5,8 +5,6 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
-// Keep this until https://github.com/rust-lang/rust-bindgen/issues/1651 is fixed.
-#![cfg_attr(test, allow(deref_nullptr))]
 
 #[allow(clippy::undocumented_unsafe_blocks)]
 pub mod vfio;
